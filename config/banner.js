90c3a2bfb77cc9bb5c5e3eb2065cdf99ff6758c6
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-15 23:16:26 +0800

    Update banner.js

--- config/banner.js
+++ config/banner.js
@@ -4,23 +4,26 @@ const { ColorTheme } = require("./colors");
 const colors = new ColorTheme();
 
 function displayBanner() {
+  // 使用 figlet 库生成横幅文字
   const banner = figlet.textSync("Humanity BOT", {
-    font: "ANSI Shadow",
-    horizontalLayout: "default",
-    verticalLayout: "default",
-    width: 100,
+    font: "ANSI Shadow", // 设置字体
+    horizontalLayout: "default", // 水平布局方式
+    verticalLayout: "default", // 垂直布局方式
+    width: 100, // 设置横幅宽度
   });
 
-  console.log(colors.style(banner, "header"));  // 显示横幅
+  // 输出横幅的样式
+  console.log(colors.style(banner, "header"));
   console.log(
-    colors.style("===============================================", "border")  // 边框
+    colors.style("===============================================", "border") // 输出分隔符
   );
-  console.log(colors.style("编写：qklxsqf", "link"));  // GitHub 链接
-  console.log(colors.style("电报频道：https://t.me/ksqxszq", "link"));  // Telegram 链接
+  console.log(colors.style("编写者: 子清", "link")); // 输出编写者信息
+  console.log(colors.style("电报频道: https://t.me/ksqxszq", "link")); // 输出电报频道链接
   console.log(
-    colors.style("===============================================\n", "border")  // 边框
+    colors.style("===============================================\n", "border") // 输出分隔符
   );
 }
 
 module.exports = displayBanner;
+
 

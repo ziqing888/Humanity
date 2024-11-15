70874e39c902f38a669ae3beabce93bc81a69c65
子清 <120438383+ziqing888@users.noreply.github.com>
2024-11-15 21:57:24 +0800

    Update banner.js

--- config/banner.js
+++ config/banner.js
@@ -1,15 +1,23 @@
-import chalk from "chalk"; 
+import figlet from "figlet"; 
+import { ColorTheme } from "./colors.js"; // 确保正确导入路径和扩展名
 
-import { ColorTheme } from "./config/colors"; 
 const colors = new ColorTheme();
 
-function displayHeader() {
-    console.log(colors.style('╔════════════════════════════════════════╗', 'border'));
-    console.log(colors.style('║      🚀   自动每日领取 $RWT 🚀         ║', 'header'));
-    console.log(colors.style('║  👤    脚本编写：子清                  ║', 'header'));
-    console.log(colors.style('║  📢  电报频道：https://t.me/ksqxszq    ║', 'link'));
-    console.log(colors.style('╚════════════════════════════════════════╝', 'border'));
-    console.log();  // 添加额外空行以分隔内容
+function displayBanner() {
+  const banner = figlet.textSync("Humanity BOT", {
+    font: "ANSI Shadow",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 100,
+  });
+
+  // 更新为新的文本内容
+  console.log(colors.style('╔════════════════════════════════════════╗', 'border'));
+  console.log(colors.style('║      🚀   自动每日领取 $RWT 🚀         ║', 'header'));
+  console.log(colors.style('║  👤    脚本编写：子清                  ║', 'header'));
+  console.log(colors.style('║  📢  电报频道：https://t.me/ksqxszq    ║', 'link'));
+  console.log(colors.style('╚════════════════════════════════════════╝', 'border'));
 }
 
-export default displayHeader; // 使用 export 替代 module.exports
+export default displayBanner; // 使用 export 导出
+
